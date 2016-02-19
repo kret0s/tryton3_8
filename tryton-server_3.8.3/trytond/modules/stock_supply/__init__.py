1719095b2a726d4b5dc505e7dc0df45d662163d7
Keyur Bhatt <keyur.bhatt.serpentcs@gmail.com>
2016-02-19 19:10:42 +0530

    [ADD: Latest version of tryton-server 3.8]

---
+++ tryton-server_3.8.3/trytond/modules/stock_supply/__init__.py
@@ -0,0 +1,31 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from .order_point import *
+from .product import *
+from .purchase_request import *
+from .shipment import *
+from .location import *
+from .purchase import *
+
+
+def register():
+    Pool.register(
+        OrderPoint,
+        Product,
+        PurchaseRequest,
+        CreatePurchaseRequestStart,
+        CreatePurchaseAskParty,
+        HandlePurchaseCancellationExceptionStart,
+        ShipmentInternal,
+        CreateShipmentInternalStart,
+        Location,
+        Purchase,
+        module='stock_supply', type_='model')
+    Pool.register(
+        CreatePurchaseRequest,
+        CreatePurchase,
+        CreateShipmentInternal,
+        HandlePurchaseCancellationException,
+        module='stock_supply', type_='wizard')
